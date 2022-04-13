a9416a4b7a8ab47cf7ef4727841216e08d0932c6
Khusniddin Nurmamatov <93917817+Khusniddin2004@users.noreply.github.com>
2022-04-14 00:04:26 +0500

    Added main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"tasks/Instagram_clone/insta_api/api"
+	"tasks/Instagram_clone/insta_api/config"
+	"tasks/Instagram_clone/insta_api/pkg/logger"
+	"tasks/Instagram_clone/insta_api/services"
+)
+
+func main() {
+
+	cfg := config.Load()
+	log := logger.New(cfg.LogLevel, "api_gateway")
+
+	serviceManager, err := services.NewServiceManager(&cfg)
+	if err != nil {
+		log.Error("gRPC dial error", logger.Error(err))
+	}
+
+	server := api.New(api.Option{
+		Conf:           cfg,
+		Logger:         log,
+		ServiceManager: serviceManager,
+	})
+
+	serviceManager.UserService()
+	if err := server.Run(cfg.Port); err != nil {
+		log.Fatal("failed to run http server", logger.Error(err))
+		panic(err)
+	}
+
+}
