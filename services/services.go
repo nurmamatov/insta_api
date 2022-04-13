df847f96858ae601e7048b150fed5fd284ed2fbd
Khusniddin Nurmamatov <93917817+Khusniddin2004@users.noreply.github.com>
2022-04-14 00:05:59 +0500

    Fixed and added sevices.go

--- services/services.go
+++ services/services.go
@@ -1,1 +1,67 @@
 package services
+
+import (
+	"fmt"
+
+	"tasks/Instagram_clone/insta_api/config"
+	pc "tasks/Instagram_clone/insta_api/genproto/comment_proto"
+	pp "tasks/Instagram_clone/insta_api/genproto/post_proto"
+	pu "tasks/Instagram_clone/insta_api/genproto/user_proto"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/resolver"
+)
+
+type IServiceManager interface {
+	UserService() pu.UserServiceClient
+	PostService() pp.PostServiceClient
+	CommentService() pc.CommentServiceClient
+}
+
+type serviceManager struct {
+	postService    pp.PostServiceClient
+	userService    pu.UserServiceClient
+	commentService pc.CommentServiceClient
+}
+
+func NewServiceManager(conf *config.Config) (IServiceManager, error) {
+	resolver.SetDefaultScheme("dns")
+
+	connPost, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", conf.PostServiceHost, conf.PostServicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+
+	connUser, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", conf.UserServiceHost, conf.UserServicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	connComment, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", conf.CommentServiceHost, conf.CommentServicePort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	serviceManager := &serviceManager{
+		postService:    pp.NewPostServiceClient(connPost),
+		userService:    pu.NewUserServiceClient(connUser),
+		commentService: pc.NewCommentServiceClient(connComment),
+	}
+
+	return serviceManager, nil
+}
+
+func (s *serviceManager) PostService() pp.PostServiceClient {
+	return s.postService
+}
+func (s *serviceManager) UserService() pu.UserServiceClient {
+	return s.userService
+}
+func (s *serviceManager) CommentService() pc.CommentServiceClient {
+	return s.commentService
+}
